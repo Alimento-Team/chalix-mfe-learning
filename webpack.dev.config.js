0f206c6bf6618d93e00610a6c1eea226ade21deb
dungdl <dungdl0298@gmail.com>
2025-10-07 06:02:57 +0700

    feat: update webpack configuration to ensure correct resolution of entities module

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -6,6 +6,9 @@ const config = createConfig('webpack-dev');
 config.resolve.alias = {
   ...config.resolve.alias,
   '@src': path.resolve(__dirname, 'src'),
+  // Force entities to resolve to the correct location
+  'entities': path.resolve(__dirname, 'node_modules/entities'),
+  'entities/lib/escape.js': path.resolve(__dirname, 'node_modules/entities/lib/escape.js'),
 };
 
 config.resolve.modules = [
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -18,6 +18,9 @@ config.plugins.push(
 config.resolve.alias = {
   ...config.resolve.alias,
   '@src': path.resolve(__dirname, 'src'),
+  // Force entities to resolve to the correct location
+  'entities': path.resolve(__dirname, 'node_modules/entities'),
+  'entities/lib/escape.js': path.resolve(__dirname, 'node_modules/entities/lib/escape.js'),
 };
 
 config.resolve.modules = [
